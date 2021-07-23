f42ca40f365e23e019aaa65a9c68b1247d5e9490
JoeMilius <joemilius@gmail.com>
2021-07-23 16:15:08 -0600

    Completed Assignment

---
+++ db/migrate/20210723214118_make_artists.rb
@@ -0,0 +1,10 @@
+class MakeArtists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :artists do |table|
+      table.string :name
+      table.string :genre
+      table.integer :age
+      table.string :hometown
+    end
+  end
+end
